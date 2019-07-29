6ee7490dc39c61b82aa5c9a8c365b91ba2b3ddce
Purushothaman S <33724283+Darth-Kronos@users.noreply.github.com>
2019-07-29 14:09:38 +0530

    Update blog.py

--- codefundo/Blog/blog.py
+++ codefundo/Blog/blog.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import LoginForm, VoterForm
 import mysql.connector
-from . import app
+
 
 Username = ''
 
-#app = Flask(__name__)
+app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 @app.route("/")
 def home():
     return render_template('home.html')
-"""
+
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -52,4 +52,3 @@ def vote():
 
 if __name__ == '__main__':
     app.run(debug=True)
-"""
